a28db935ff992ec5b80632e3a41595257f873be4
Jenny Wagner <jennywagg@gmail.com>
2014-12-01 01:07:18 -0500

    Moved miniD

--- src/WordSort.java
+++ src/WordSort.java
@@ -1,12 +1,17 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class WordSort {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         if (args.length > 0) {
             File miniDict = new File(args[0]);
+            Scanner in = new Scanner(miniDict);
 
+            String line = in.nextLine();
+            System.out.println(line);
+            in.close();
         }
 
-        System.out.println("Hi");
     }
 }
