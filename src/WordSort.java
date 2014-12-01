81e355daf9a649d3df738d555596d91e64a188c5
Jenny Wagner <jennywagg@gmail.com>
2014-12-01 01:33:11 -0500

    WordSort takes file arguments

--- src/WordSort.java
+++ src/WordSort.java
@@ -1,16 +1,22 @@
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.Scanner;
 
 public class WordSort {
     public static void main(String[] args) throws FileNotFoundException {
-        if (args.length > 0) {
+        if (args.length == 3) {
             File miniDict = new File(args[0]);
-            Scanner in = new Scanner(miniDict);
+            Scanner dictIn = new Scanner(miniDict);
+            File unsorted = new File(args[1]);
+            OutputStream out = new BufferedOutputStream(new FileOutputStream(
+                    args[2]));
 
-            String line = in.nextLine();
-            System.out.println(line);
-            in.close();
+            dictIn.close();
+        } else {
+            System.out.println("Please enter three arguments");
         }
 
     }
