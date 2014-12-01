a86138b079f03fbed17822a5cec1c45f6b3fe8e0
Jenny Wagner <jennywagg@gmail.com>
2014-12-01 00:34:36 -0500

    Started WordSort
    Beginning of WordSort, makes miniDict out of first arg

---
+++ src/WordSort.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+public class WordSort {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            File miniDict = new File(args[0]);
+
+        }
+    }
+}
